678a8c5a1d1a849dd270a0dd4d9ce678d42c214d
alessandroschiano98 <schianoski98@gmail.com>
2025-10-18 12:54:59 +0200

    add lightbox img & lock save option

--- js/works.js
+++ js/works.js
@@ -4,3 +4,31 @@ document.addEventListener("DOMContentLoaded", () => {
       setTimeout(() => el.classList.add("show"), i * 300);
     });
   });
+
+
+
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = lightbox.querySelector('img');
+
+const images = document.querySelectorAll('.clickable-img');
+
+
+images.forEach(img => {
+  img.addEventListener('click', () => {
+    lightboxImg.src = img.src; 
+    lightbox.classList.add('show'); 
+  });
+});
+
+
+lightbox.addEventListener('click', () => {
+  lightbox.classList.remove('show');
+});
+
+// ! BLOCCO TASTO DESTRO PER EVITARE SALVATAGGI 
+const imgs = document.querySelectorAll('.visualCard img, .lightbox img');
+
+imgs.forEach(img => {
+  img.addEventListener('contextmenu', e => e.preventDefault());
+});
+
