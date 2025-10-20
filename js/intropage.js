bd27c62003408f02a5f9bdbd3ef9a7aa1bd5a036
alessandroschiano98 <schianoski98@gmail.com>
2025-10-20 13:23:51 +0200

    others fix

--- js/intropage.js
+++ js/intropage.js
@@ -1,8 +1,7 @@
 setTimeout(() => {
-    document.body.style.transition = "opacity 1s";
-    document.body.style.opacity = "0";
-    setTimeout(() => {
-      window.location.href = "/html/homepage.html";
-    }, 1000);
-  }, 3000);
-  
+  document.body.style.transition = "opacity 1s";
+  document.body.style.opacity = "1";
+  setTimeout(() => {
+    window.location.href = "/html/homepage.html";
+  }, 1000);
+}, 3000);
