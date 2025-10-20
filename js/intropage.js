904542daecc2d2a28c82e00e64d08119e302b496
alessandroschiano98 <schianoski98@gmail.com>
2025-10-20 13:31:31 +0200

    intro fix

--- js/intropage.js
+++ js/intropage.js
@@ -1,6 +1,6 @@
 setTimeout(() => {
   document.body.style.transition = "opacity 1s";
-  document.body.style.opacity = "1";
+  document.body.style.opacity = "0";
   setTimeout(() => {
     window.location.href = "/html/homepage.html";
   }, 1000);
