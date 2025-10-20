37d61e749e695eb4006fc5dee3d5a7158ff02cb7
alessandroschiano98 <schianoski98@gmail.com>
2025-10-20 12:41:44 +0200

    fix deploy hosting

--- js/intropage.js
+++ js/intropage.js
@@ -2,7 +2,7 @@ setTimeout(() => {
     document.body.style.transition = "opacity 1s";
     document.body.style.opacity = "0";
     setTimeout(() => {
-      window.location.href = "homepage.html";
+      window.location.href = "/html/homepage.html";
     }, 1000);
   }, 3000);
   
