5d1e1e3fc10d8a860ba9335d02626b74732eaa20
alessandroschiano98 <schianoski98@gmail.com>
2025-10-20 15:39:44 +0200

    fixed id burgermenu

--- js/homepage.js
+++ js/homepage.js
@@ -1,43 +1,48 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const homepageClick = document.getElementById("homepage-click");
-    if (homepageClick) {
-      homepageClick.addEventListener("click", (event) => {
-        event.preventDefault();
-        window.location.href = "/html/homepage.html";
-      });
-    }
-  
-    const linkClick = document.getElementById("works-click");
-    if (linkClick) {
-      linkClick.addEventListener("click", (event) => {
-        event.preventDefault();
-        window.location.href = "/html/works.html";
-      });
-    }
-  
-    const aboutmeClick = document.getElementById("aboutme-click");
-    if (aboutmeClick) {
-      aboutmeClick.addEventListener("click", (event) => {
-        event.preventDefault();
-        window.location.href = "/html/aboutme.html";
-      });
-    }
-  
-    const homepageCentre = document.querySelector(".homepagecentre");
-    if (homepageCentre) {
-      homepageCentre.classList.add("show");
-    }
-  
-    const hamburger = document.querySelector('.hamburger-checkbox');
-    const navLinks = document.querySelector('.navbar-links');
-    if (hamburger && navLinks) {
-      hamburger.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-      });
-    }
-  
-    window.addEventListener("load", () => {
-      document.body.classList.add("loaded");
+  // ! Link homepage
+  const homepageClick = document.getElementById("homepage-click");
+  if (homepageClick) {
+    homepageClick.addEventListener("click", (event) => {
+      event.preventDefault();
+      window.location.href = "/html/homepage.html";
     });
+  }
+
+  // ! Link works
+  const linkClick = document.getElementById("works-click");
+  if (linkClick) {
+    linkClick.addEventListener("click", (event) => {
+      event.preventDefault();
+      window.location.href = "/html/works.html";
+    });
+  }
+
+  // ! Link about me
+  const aboutmeClick = document.getElementById("aboutme-click");
+  if (aboutmeClick) {
+    aboutmeClick.addEventListener("click", (event) => {
+      event.preventDefault();
+      window.location.href = "/html/aboutme.html";
+    });
+  }
+
+  // ! Animazione homepagecentre
+  const homepageCentre = document.querySelector(".homepagecentre");
+  if (homepageCentre) {
+    homepageCentre.classList.add("show");
+  }
+
+  // ! Hamburger menu toggle
+  const hamburger = document.getElementById("hamburger-checkbox");
+  const navLinks = document.querySelector(".navbar-links");
+  if (hamburger && navLinks) {
+    hamburger.addEventListener("change", () => {
+      navLinks.classList.toggle("active");
+    });
+  }
+
+  // ! Body loaded
+  window.addEventListener("load", () => {
+    document.body.classList.add("loaded");
   });
-  
+});
