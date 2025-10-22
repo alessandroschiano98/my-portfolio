297fdccd6a030423450b60dff37948676b2d5ad0
alessandroschiano98 <schianoski98@gmail.com>
2025-10-22 15:07:34 +0200

    fix replace img (75% - weight mbyte)

--- js/works.js
+++ js/works.js
@@ -1,34 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const elements = document.querySelectorAll(".fade-in");
-    elements.forEach((el, i) => {
-      setTimeout(() => el.classList.add("show"), i * 300);
+  // --------------------------
+  // ! FADE-IN PER LE CARD
+  // --------------------------
+  const fadeElements = document.querySelectorAll(".fade-in, .visualCard");
+  fadeElements.forEach((el, i) => {
+    setTimeout(() => el.classList.add("show"), i * 200);
+  });
+
+  // --------------------------
+  // ! LIGHTBOX
+  // --------------------------
+
+  const lightbox = document.getElementById('lightbox');
+  const lightboxImg = lightbox.querySelector('img');
+
+  document.querySelectorAll('.clickable-img').forEach(img => {
+    img.addEventListener('click', () => {
+      lightboxImg.src = img.src;
+      lightbox.classList.add('show');
     });
   });
 
-
-
-const lightbox = document.getElementById('lightbox');
-const lightboxImg = lightbox.querySelector('img');
-
-const images = document.querySelectorAll('.clickable-img');
-
-
-images.forEach(img => {
-  img.addEventListener('click', () => {
-    lightboxImg.src = img.src; 
-    lightbox.classList.add('show'); 
+  lightbox.addEventListener('click', (e) => {
+    if (e.target === lightbox) {
+      lightbox.classList.remove('show');
+    }
   });
+
 });
-
-
-lightbox.addEventListener('click', () => {
-  lightbox.classList.remove('show');
-});
-
-// ! BLOCCO TASTO DESTRO PER EVITARE SALVATAGGI 
-const imgs = document.querySelectorAll('.visualCard img, .lightbox img');
-
-imgs.forEach(img => {
-  img.addEventListener('contextmenu', e => e.preventDefault());
-});
-
